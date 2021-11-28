3442ba653dfa2dbd358f999661f8fefc4b4e7000
wiem dakhli <wiemdakhli7@gmail.com>
2021-11-29 00:54:15 +0100

    page accueil

--- src/app/components/accueil/accueil.component.ts
+++ src/app/components/accueil/accueil.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accueil.component.css']
 })
 export class AccueilComponent implements OnInit {
+  destinations:string[]=["Italie","Londres","Corée du sud"];
+  datesA:Date[]=[new Date(2021,12,5),new Date(2022,1,14),new Date(2022,2,30)];
+  datesR:Date[]=[new Date(2021,12,19),new Date(2022,1,21),new Date(2022,3,5)];
 
   constructor() { }
 
