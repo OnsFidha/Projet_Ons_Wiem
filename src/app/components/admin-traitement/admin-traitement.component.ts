8b2831d489560dab9c9bfdc429c43625c51022b2
wiem dakhli <wiemdakhli7@gmail.com>
2021-12-12 21:08:54 +0100

    validators admin-trait ts et html

--- src/app/components/admin-traitement/admin-traitement.component.ts
+++ src/app/components/admin-traitement/admin-traitement.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormGroup ,FormBuilder, FormControl} from '@angular/forms';
+import { FormArray, FormGroup ,FormBuilder, FormControl, Validators} from '@angular/forms';
 import { Voyage } from 'src/app/model/voyage';
 import { VoyageService } from 'src/app/services/voyage.service';
 
@@ -48,20 +48,60 @@ export class AdminTraitementComponent implements OnInit {
       this.lesVilles.push(this.fb.control(''));
     }
 
+    get image(){
+      return this.voyageForm.get('img');
+    }
+    get destination(){
+      return this.voyageForm.get('destination');
+    }
+    get continent(){
+      return this.voyageForm.get('continent');
+    }
+    get dateAller(){
+      return this.voyageForm.get('dateAller');
+    }
+    get dateRetour(){
+      return this.voyageForm.get('dateRetour');
+    }
+    get planning(){
+      return this.voyageForm.get('planning');
+    }
+    get hotel(){
+      return this.voyageForm.get('hotel');
+    }
+    get nbJ(){
+      return this.voyageForm.get('nbJ');
+    }
+    get prix(){
+      return this.voyageForm.get('prix');
+    }
+    get etoile(){
+      return this.voyageForm.get('etoile');
+    }
+    isValidPattern():boolean{
+      return this.voyageForm.controls['etoile'].errors?.['pattern'] &&
+      this.voyageForm.controls['etoile'].dirty;
+      }
+      isValidPattern2():boolean{
+        return this.voyageForm.controls['nbJ'].errors?.['pattern'] &&
+        this.voyageForm.controls['nbJ'].dirty;
+        }
+     
+
   ngOnInit(): void {
     this.voyageForm = this.fb.group({
 
-        img:["lien image"],
-        destination:["destination"],
-        continent :["continent"],
-        dateAller: ["01/01/2001"],
-        dateRetour: ["01/01/2080"],
-        nbJ:[0],
-        prix : [111],
+        img:["",Validators.required],
+        destination:["",Validators.required],
+        continent :["",Validators.required],
+        dateAller: ["",Validators.required],
+        dateRetour: ["",Validators.required],
+        nbJ:[Validators.required, Validators.pattern('^([1-9]||[0-9][0-9])$')],
+        prix : [Validators.required],
         enPromo: [false],
-        planning:["planning"],
-        hotel:["hotel"],
-        etoile:[0],
+        planning:["",Validators.required],
+        hotel:["",Validators.required],
+        etoile:[Validators.required, Validators.pattern('^([1-5])$')],
         villes:this.fb.array([])
     })    
     
